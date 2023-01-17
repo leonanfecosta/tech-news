9efeb0d0cccef0cc3c453636f1ed2a96b3106acd
Leonan Costa <leonanfecosta@gmail.com>
2023-01-17 15:58:43 -0300

    feat: desenvolvido função fetch

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,18 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    try:
+        time.sleep(1)
+        response = requests.get(
+            url, headers={"user-agent": "Fake user-agent"}, timeout=3
+        )
+        if response.status_code == 200:
+            return response.text
+    except (requests.HTTPError, requests.ReadTimeout):
+        return None
 
 
 # Requisito 2
