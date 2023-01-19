0719b34d01712cd657a7ed0858f150617e6f3bb5
Leonan Costa <leonanfecosta@gmail.com>
2023-01-19 09:24:05 -0300

    feat: desenvolvido função top_5_news

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,16 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    news_most_commented = sorted(
+        news, key=lambda x: x["comments_count"], reverse=True
+    )
+    top_5_news = []
+    for notice in news_most_commented[:5]:
+        top_5_news.append((notice["title"], notice["url"]))
+    return top_5_news
 
 
 # Requisito 11
